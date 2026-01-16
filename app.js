883812279b5a0cef51d003463cff5529209040f6
luchoangel31 <66985513+luchoangel31@users.noreply.github.com>
2026-01-15 22:29:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const indexRouter = require('./routes/index');
 
 const app = express();
-const PORT = 3000;
+app.use(express.json())
+const PORT = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,6 +13,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Use the router for handling routes
 app.use('/', indexRouter);
 
+app.get('/health', (req, res) => {
+  res.send('API funcionando 🚀')
+})
 // Catch-all route for handling 404 errors
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
